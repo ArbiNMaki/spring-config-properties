12087f511b7369fefd8168bf49146b79e1b1557c
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-07-17 13:24:25 +0700

    Resource

---
+++ src/main/java/com/spring/config/LearnConfigApplication.java
@@ -0,0 +1,13 @@
+package com.spring.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnConfigApplication.class, args);
+	}
+
+}
